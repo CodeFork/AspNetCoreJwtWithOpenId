bdbe0cc3324959c6c41095e8a8f2e0785de9e962
Asad Sahi <asadazmat@gmail.com>
2016-07-23 20:07:04 +0100

    working with  vscode and sql lite

--- Migrations/20160723184224_Initial.Designer.cs
+++ Migrations/20160723184224_Initial.Designer.cs
@@ -8,9 +8,10 @@ using openiddicttest.Models;
 namespace openiddicttest.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160723184224_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
